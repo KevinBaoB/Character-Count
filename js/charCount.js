d263a07ab53475a48f1352d61bd8210b94978c8e
KevinBaoB <kevinbaobui@gmail.com>
2022-05-27 10:20:19 -0500

    First

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,19 @@
-exports.charCount = function() {
+exports.charCount = function (word) {
+  let arr = [];
+  word = word.split("");
+  let uniqueArr = [...new Set(word.sort())].filter((w) => w != " ");
 
+  for (let i of uniqueArr) {
+    let charCount = 0;
+    for (let j of word) {
+      if (j === i) {
+        charCount += 1;
+      }
+    }
+    arr.push([i, charCount]);
+  }
+  return arr.sort((a, b) => b[1] - a[1]);
 };
+
+// console.log(charCount("aaabbc"));
+// console.log(charCount("an apple a day will keep the doctor away"));
